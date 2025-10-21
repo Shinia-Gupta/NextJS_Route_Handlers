4a69078851b083deaefb12a431f4ddee53a0d453
Shinia-Gupta <shiniavit@gmail.com>
2025-10-21 15:02:07 +0530

    Simple REST POST request in Next.js

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -3,3 +3,18 @@ import { comments } from "./data";
 export async function GET() {
     return Response.json(comments);
 }
+
+export async function POST(request: Request) {
+    const { text } = await request.json();
+    const newComment = {
+        id: comments.length + 1,
+        text,
+    };
+    comments.push(newComment);
+    // return Response.json(newComment, { status: 201 });
+    //OR
+    return new Response(JSON.stringify(newComment), {
+        status: 201,
+        headers: { "Content-Type": "application/json" },
+    });
+}
