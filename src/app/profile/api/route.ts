9b248431a94f4daae40eefef3b848a5ac568df47
Shinia-Gupta <shiniavit@gmail.com>
2025-10-21 17:59:04 +0530

    Way 2- setting and getting cookies

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request:NextRequest) {
@@ -12,6 +12,11 @@ export async function GET(request:NextRequest) {
   
   const theme=request.cookies.get("theme");
   console.log("Current theme:",theme);
+
+  const cookieStore=await cookies();
+  cookieStore.set("resultsPerPage","10");
+  console.log("Set-Cookie Header:",cookieStore.get("resultsPerPage"));
+  
   // return new Response('Hello, Profile Page! This route file takes over from the page.tsx file both being on the same route and same level. So in case page.tsx and route.ts files are present on the same level for a folder, route.ts takes over from page.tsx fole');
   return new Response("<h1>Profile data</h1>",{
     headers:{
