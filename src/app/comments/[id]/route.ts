279e1da0dac77269bbbb531a6d429224ed79ff73
Shinia-Gupta <shiniavit@gmail.com>
2025-10-21 16:38:07 +0530

    Simple REST PATCH request in Next.js

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -6,3 +6,16 @@ const {id}=await params
 const comment=comments.find(c=>c.id===parseInt(id))
 return Response.json(comment)
 }
+
+export async function PATCH(request: Request, { params }: { params: Promise<{ id: string }> }) {
+
+    const {id}=await params
+    const requestBody=await request.json()
+    const commentIndex=comments.findIndex(c=>c.id===parseInt(id))
+    if(commentIndex>-1){
+        comments[commentIndex]={...comments[commentIndex],...requestBody}
+        return Response.json(comments[commentIndex])
+    }else{
+        return new Response("Comment not found", {status:404})
+    }
+}
