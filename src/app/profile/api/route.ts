1210e500c90c91c779dd29210ae17693fdc690b5
Shinia-Gupta <shiniavit@gmail.com>
2025-10-21 17:46:20 +0530

    handling Headers in route handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,19 @@
-export async function GET() {
-  return new Response('Hello, Profile Page! This route file takes over from the page.tsx file both being on the same route and same level. So in case page.tsx and route.ts files are present on the same level for a folder, route.ts takes over from page.tsx fole');
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request:NextRequest) {
+  // const reqestHeaders=new Headers(request.headers);
+  // console.log("Request Headers:",reqestHeaders.get("Authorization"));
+
+  //OR
+
+  const headersList=await headers();
+  console.log("Request Headers:",headersList.get("Authorization"));
+  
+  // return new Response('Hello, Profile Page! This route file takes over from the page.tsx file both being on the same route and same level. So in case page.tsx and route.ts files are present on the same level for a folder, route.ts takes over from page.tsx fole');
+  return new Response("<h1>Profile data</h1>",{
+    headers:{
+      "Content-Type":"text/html"
+    }
+  })
 }
